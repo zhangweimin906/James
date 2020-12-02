d441af301dd2db931297f8e58d0d60c6d78052c0
zhangweimin906 <zhangweimin906@163.com>
2020-12-02 10:41:06 +0800

    Update Program.cs

---
+++ Demo/James.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+  public  class James
+    {
+        public void ShowData()
+        {
+            Console.WriteLine("James");
+        }
+        
+    }
+}
